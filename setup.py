07acef5cdabea5c219a83456a46456d1a765ec31
Jonathan Tushman <jtushman@pipewave.com>
2013-11-06 18:02:00 -0800

    supporting lists

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='dict_digger',
-      version='0.1.1',
-      description='Useful syntax for digging into nested dictionaries, and removes the need to check if a key exists',
+      version='0.2.1',
+      description='Useful syntax for digging into nested dictionaries, lists and tuples, and removes the need to check if a key exists',
       url='https://github.com/jtushman/dict_digger',
       author='Jonathan Tushman',
       author_email='jonathan@zefr.com',
--- test_dict_digger.py
+++ test_dict_digger.py
@@ -1,5 +1,6 @@
 import nose
 from nose.tools import *
+from nose.tools import assert_raises
 import dict_digger
 
 
@@ -7,6 +8,26 @@ TEST_HASH = {
     'a': {
         'b': 'tuna',
         'c': 'fish'
+    },
+    'b': {}
+}
+
+TEST_LIST = [
+    {
+        'b': 'tuna',
+        'c': 'fish'
+    },
+    {}
+]
+
+TEST_COMPLEX = {
+    'a': {
+        'b': 'tuna',
+        'c': 'fish',
+        'list': [
+            {'x': 'peanut butter'},
+            {'y': 'jelly'},
+        ]
     },
     'b': {}
 }
@@ -30,6 +51,29 @@ def test_nested_miss():
 def test_end_point_miss():
     result = dict_digger.dig(TEST_HASH,'a','z')
     eq_(None, result)
+
+
+def test_complex_find():
+    result = dict_digger.dig(TEST_COMPLEX,'a','list',0)
+    assert isinstance(result,dict)
+    result = dict_digger.dig(TEST_COMPLEX,'a','list',0,'x')
+    eq_('peanut butter', result)
+
+
+def test_list_find():
+    result = dict_digger.dig(TEST_LIST,0,'b')
+    eq_("tuna", result)
+
+
+def test_no_exception():
+    result = dict_digger.dig(TEST_LIST,0,'b','c')
+    assert result is None
+
+@raises(IndexError)
+def test_exception():
+    dict_digger.dig(TEST_LIST,0,'b','c', fail=True)
+
+
 
 
 if __name__ == "__main__":
